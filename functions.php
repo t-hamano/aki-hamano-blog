cf4e87a392b1216b60f2905c418c3022f0b7010a
Aki Hamano <tetsuaki.hamano@gmail.com>
2024-06-22 19:38:41 +0900

    Add prism.js, adjust headings and code style

--- functions.php
+++ functions.php
@@ -2,10 +2,21 @@
 // CSSのエンキュー
 function aki_hamano_blog_wp_enqueue_scripts() {
 	wp_enqueue_style(
-		'aki-hamano-blog-style',
+		'aki-hamano-blog',
 		get_stylesheet_uri(),
 		array(),
 		filemtime( get_stylesheet_directory() . '/style.css' )
+	);
+
+	wp_enqueue_style(
+		'aki-hamano-blog-prism',
+		get_stylesheet_directory_uri() . '/assets/lib/prism/prism.css',
+		array(),
+	);
+	wp_enqueue_script(
+		'aki-hamano-blog',
+		get_stylesheet_directory_uri() . '/assets/lib/prism/prism.js',
+		array(),
 	);
 }
 add_action( 'wp_enqueue_scripts', 'aki_hamano_blog_wp_enqueue_scripts' );
